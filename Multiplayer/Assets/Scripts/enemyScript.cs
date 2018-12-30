210cd2d2bb2fa25764f3f8e28fb67bb9a56d6fd6
SlimShaney <44029427+SlimShaney@users.noreply.github.com>
2018-12-30 15:52:44 +0000

    Second Commit

---
+++ Multiplayer/Assets/Scripts/enemyScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyScript : MonoBehaviour {
+    /*
+    private UnityEngine.AI.NavMeshAgent m_NavMeshAgent;
+    private int m_CurrentWaypoint;
+
+    [SerializeField] private Transform[] m_Waypoints;
+
+    void Start()
+    {
+
+        m_CurrentWaypoint = 0;
+
+        m_NavMeshAgent.updatePosition = false;
+        m_NavMeshAgent.updateRotation = true;
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        CheckPlayer();
+        m_NavMeshAgent.nextPosition = transform.position;
+
+        switch (m_NPCState)
+        {
+            case NPCState.CHASE:
+                Chase();
+                break;
+            case NPCState.PATROL:
+                Patrol();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void Patrol()
+    {
+        //Debug.Log("Patrolling");
+
+        CheckWaypointDistance();
+        m_NavMeshAgent.SetDestination(m_Waypoints[m_CurrentWaypoint].position);
+    }
+
+    void CheckWaypointDistance()
+    {
+        if (Vector3.Distance(m_Waypoints[m_CurrentWaypoint].position, this.transform.position) < m_ThresholdDistance)
+        {
+            m_CurrentWaypoint = (m_CurrentWaypoint + 1) % m_Waypoints.Length;
+        }
+    }*/
+}
